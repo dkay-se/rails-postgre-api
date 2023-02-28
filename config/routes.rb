a3e8b987ef70162ec2f59183b18d1897c5fc443a
Sergio Peralta <v00173045@pirates.victoriacollege.edu>
2023-02-28 18:20:24 -0300

    controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resource :users, only: %i[create] # for signing up
+  resources :sessions, only: %i[create destroy] # for logging in and out
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/login', to: 'users#login'
+  get '/auto_login', to: 'users#auto_login'
 end
